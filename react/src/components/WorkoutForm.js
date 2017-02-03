8bfe764f2909f99edff390492abfcc2bffd2b5a0
wjfletcher <warrenjfletcher@gmail.com>
2017-02-03 14:47:18 -0500

    more react form hell

--- react/src/components/WorkoutForm.js
+++ react/src/components/WorkoutForm.js
@@ -5,6 +5,27 @@ class WorkoutForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log(this.refs.reps.value);
+
+    let date = this.refs.date.value;
+    let exercise = this.refs.exercise.value;
+    let reps = this.refs.reps.value;
+    let sets = this.refs.sets.value;
+    let weight = this.refs.weight.value;
+
+    fetch(`/api/v1/workouts`, {
+      credentials: 'same-origin',
+      method: 'post',
+      data: {workout: {date: date, exercise: exercise, reps: reps, sets: sets, weight: weight}},
+      success: (response) => {
+        console.log('it worked!', response);
+      }
+    });
   }
 
   render() {
@@ -12,28 +33,51 @@ class WorkoutForm extends Component {
       <div className="row center-text">
       <div className="form-element center">
         <form method="post" action="/workouts" >
+
         <input name="authenticity_token" value={this.props.formAuth} type="hidden" />
-        <select name="workout[exercise_id]" id="workout_exercise_id">
+
+        <div className="row">
+        <div className="small-12 large-6 columns">
+        <div className="field">
+        <label htmlFor="workout_date">Date</label>
+        <input type="date" name="workout[date]" id="workout_date" ref="date" />
+        </div>
+        </div>
+
+        <div className="small-12 large-6 columns">
+        <div className="field">
+        <label htmlFor="workout_exercise_id">Exercise</label>
+        <select name="workout[exercise_id]" id="workout_exercise_id" ref="exercise" >
           {this.props.dropDown}
         </select>
-          <label htmlFor="workout_date">Date</label>
-          <input type="date" name="workout[date]" id="workout_date" />
+        </div>
+        </div>
+        </div>
+
+        <div className="row">
+        <div className="small-12 large-4 columns">
         <div className="field">
           <label htmlFor="workout_reps">Reps</label>
-          <input type="number" name="workout[reps]" id="workout_reps" />
+          <input type="number" name="workout[reps]" id="workout_reps" ref="reps" />
         </div>
-        <br />
+        </div>
+
+        <div className="small-12 large-4 columns">
         <div className="field">
           <label htmlFor="workout_sets">Sets</label>
-          <input type="number" name="workout[sets]" id="workout_sets" />
+          <input type="number" name="workout[sets]" id="workout_sets" ref="sets" />
         </div>
-        <br />
+        </div>
+
+        <div className="small-12 large-4 columns">
         <div className="field">
           <label htmlFor="workout_weight">Weight</label>
-          <input type="text" name="workout[weight]" id="workout_weight" />
+          <input type="text" name="workout[weight]" id="workout_weight" ref="weight" />
         </div>
-        <br />
-        <input type="submit" name="commit" value="Save" />
+        </div>
+        </div>
+
+        <input type="submit" name="commit" value="Save" onClick={this.handleClick} />
         </form>
         </div>
       </div>
