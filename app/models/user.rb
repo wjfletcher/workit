c4c92ca9f17fa68daf9fb8daa18494e3d0413ead
wjfletcher <warrenjfletcher@gmail.com>
2017-02-05 09:28:31 -0500

    add google login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+
   has_many :exercises
   has_many :workouts
   has_many :exercises, through: :workouts
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(username: data["name"],
+           email: data["email"],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+end
 end
