279a78305d664246ff87c860159142dc6efbec8c
wjfletcher <warrenjfletcher@gmail.com>
2017-02-07 12:20:07 -0500

    add admin page and exercise approval

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :admin do
+    resources :exercises
+  end
+
   resources :data, only: [:index]
 
   get 'fitbit/:resource/:date.json' => 'fitbit_api#data_request'
