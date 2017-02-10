bc5720849cca3d29e1b1ca068781a4bc320ce1b4
wjfletcher <warrenjfletcher@gmail.com>
2017-02-10 17:42:40 -0500

    add images to exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,3 @@ Exercise.delete_all
 JSON.parse(open("#{Rails.root}/db/seeds/exercises.json").read).each do |exercise|
    Exercise.create(exercise)
 end
-
-User.create!(
-  email: "admin@admin.com",
-  password: "password",
-  username: "The Admin",
-  admin: true
-)
-
-
-
-# Exercise.create!([
-#     { name: "barbell curl", description: "Stand up with your torso upright while holding a barbell at a shoulder-width grip. The palm of your hands should be facing forward and the elbows should be close to the torso. This will be your starting position. ... Continue the movement until your biceps are fully contracted and the bar is at shoulder level.", category: "biceps" },
-#     { name: "cable curl", description: "Stand up with your torso upright while holding a barbell at a shoulder-width grip. The palm of your hands should be facing forward and the elbows should be close to the torso. This will be your starting position. ... Continue the movement until your biceps are fully contracted and the bar is at shoulder level.", category: "biceps" },
-#     { name: "barbell concentration curl", description: "Stand up with your torso upright while holding a barbell at a shoulder-width grip. The palm of your hands should be facing forward and the elbows should be close to the torso. This will be your starting position. ... Continue the movement until your biceps are fully contracted and the bar is at shoulder level.", category: "biceps" },
-#     { name: "hammer curl", description: "Stand up with your torso upright while holding a barbell at a shoulder-width grip. The palm of your hands should be facing forward and the elbows should be close to the torso. This will be your starting position. ... Continue the movement until your biceps are fully contracted and the bar is at shoulder level.", category: "biceps" },
-#     { name: "machine curl", description: "Stand up with your torso upright while holding a barbell at a shoulder-width grip. The palm of your hands should be facing forward and the elbows should be close to the torso. This will be your starting position. ... Continue the movement until your biceps are fully contracted and the bar is at shoulder level.", category: "biceps" }
-# ])
