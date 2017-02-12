84ce350de8f38a50f6df5a0d73af0a41f876657a
wjfletcher <warrenjfletcher@gmail.com>
2017-02-12 11:53:49 -0500

    color buttons, remove parallax

--- react/src/components/WorkoutForm.js
+++ react/src/components/WorkoutForm.js
@@ -53,12 +53,12 @@ class WorkoutForm extends Component {
 
         <div className="row">
         <div className="field small-12 large-6 columns">
-        <label htmlFor="workout_date">Date</label>
-        <input type="date" name="workout[date]" id="workout_date" ref="date" defaultValue={today}/>
+        <label htmlFor="workout_date" className="white-text">Date</label>
+        <input type="date" name="workout[date]" id="workout_date" ref="date" defaultValue={today} required="required"/>
         </div>
 
         <div className="field small-12 large-6 columns">
-        <label htmlFor="workout_exercise_id">Exercise</label>
+        <label htmlFor="workout_exercise_id" className="white-text">Exercise</label>
         <select name="workout[exercise_id]" id="workout_exercise_id" ref="exercise" >
           {this.props.dropDown}
         </select>
@@ -67,18 +67,18 @@ class WorkoutForm extends Component {
 
         <div className="row">
         <div className="field small-12 large-4 columns">
-          <label htmlFor="workout_reps">Reps</label>
-          <input type="number" name="workout[reps]" id="workout_reps" ref="reps" />
+          <label htmlFor="workout_reps" className="white-text">Reps</label>
+          <input type="number" name="workout[reps]" id="workout_reps" ref="reps" required="required" />
         </div>
 
         <div className="field small-12 large-4 columns">
-          <label htmlFor="workout_sets">Sets</label>
-          <input type="number" name="workout[sets]" id="workout_sets" ref="sets" />
+          <label htmlFor="workout_sets" className="white-text">Sets</label>
+          <input type="number" name="workout[sets]" id="workout_sets" ref="sets" required="required"/>
         </div>
 
         <div className="field small-12 large-4 columns">
-          <label htmlFor="workout_weight">Weight</label>
-          <input type="text" name="workout[weight]" id="workout_weight" ref="weight" />
+          <label htmlFor="workout_weight" className="white-text">Weight</label>
+          <input type="text" name="workout[weight]" id="workout_weight" ref="weight" required="required"/>
         </div>
         </div>
 
