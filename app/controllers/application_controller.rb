9cb600750ee7d9a4ed7d3e997e7addd0043b7dcf
wjfletcher <warrenjfletcher@gmail.com>
2017-03-09 09:06:06 -0500

    redirect to workouts on sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  def after_sign_in_path_for(resource)
+    workouts_path
+  end
 end
